af18acd79495773e9bc1969b20ac17927353bc34
abraham saldivar <saldivara1010@gmail.com>
2021-08-14 16:10:45 -0500

    commit tree 551e10f38008cb8f4479e426d0fcca23f69f0df3

---
+++ lib/song.rb
@@ -0,0 +1,30 @@
+class Song 
+     @@count = 0
+     @@artists = []
+     @@genres = []
+    attr_accessor :name , :genre , :artist
+    
+    def initialize(name, artist , genre)
+        @name = name 
+        @artist = artist 
+        @genre = genre 
+         @@count +=1 
+        @@artists << artist
+        @@genres << genre
+    end
+        def self.count
+            @@count 
+        end
+        def self.artists
+            @@artists.uniq
+        end
+        def self.genres
+            @@genres.uniq
+        end
+        def self.genre_count
+            @@genres.tally
+        end
+        def self.artist_count
+            @@artists.tally
+        end
+end
